9d924b4f24a8e0ea5b6bc4ab33e2ae78ece3b1be
Tenzin Sean <tenzin.jeunesse@gmail.com>
2019-09-02 13:55:23 +0200

    first checkup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDfckavVR-2r1NUQaZWFfuBbFCRuSQw2kE",
+    authDomain: "oshop-f28c6.firebaseapp.com",
+    databaseURL: "https://oshop-f28c6.firebaseio.com",
+    projectId: "oshop-f28c6",
+    storageBucket: "",
+    messagingSenderId: "1091569752803",
+    appId: "1:1091569752803:web:1e119385a2a11ae4"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDfckavVR-2r1NUQaZWFfuBbFCRuSQw2kE",
+    authDomain: "oshop-f28c6.firebaseapp.com",
+    databaseURL: "https://oshop-f28c6.firebaseio.com",
+    projectId: "oshop-f28c6",
+    storageBucket: "",
+    messagingSenderId: "1091569752803",
+    appId: "1:1091569752803:web:1e119385a2a11ae4"
+  }
 };
 
 /*
