4c23e53f0c6ea053e647d31e914c87fb28e08349
Tenzin Sean <tenzin.jeunesse@gmail.com>
2019-09-05 13:17:09 +0200

    added one

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {CategoryService} from '../../category.service';
 import {ProductService} from '../../product.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-product-form',
@@ -10,12 +11,16 @@ import {ProductService} from '../../product.service';
 export class ProductFormComponent implements OnInit {
   categorys$;
 
-  constructor(categoryService: CategoryService, private productService: ProductService) {
+  constructor(
+    private router: Router,
+    private categoryService: CategoryService,
+    private productService: ProductService) {
     this.categorys$ = categoryService.getCategories();
   }
 
   save(product){
     this.productService.create(product);
+    this.router.navigate(['/admin/products']);
   }
 
   ngOnInit() {
